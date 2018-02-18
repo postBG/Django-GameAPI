b4a60f8d4ba5a7b38ff50bed1814593c8009d3cd
unknown <profile2697@gmail.com>
2018-02-18 17:57:21 +0900

    [games] create Serializer

--- games/models.py
+++ games/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Game(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=200, blank=True, default='')
-    released_date = models.DateTimeField()
+    release_date = models.DateTimeField()
     game_category = models.CharField(max_length=200, blank=True, default='')
     played = models.BooleanField(default=False)
 
