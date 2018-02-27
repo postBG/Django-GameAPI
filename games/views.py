1d007af0b52720f65023582af389d6e4cb311392
unknown <profile2697@gmail.com>
2018-02-27 23:54:56 +0900

    using @api_view decorator
    - @api_view를 사용하면 내부적으로는 rest framework의 APIView클래스로 래핑해주게 된다.
    - @api_view를 쓰면 gamesapi/settings.py에서 적절한 설정을 overriding, 즉, 덮어쓰므로써 기본 파서 클래서와 기본 렌더러 클래스를 설정하게 된다.
    - 이는 rest framework의 request, response를 사용해 content-type에 따라 response나 requset의 파싱 방법이 달라짐을 의미한다.
    - DEFAULT_PARSER_CLASSES는 "application.json", "application/x-www-from-urlencoded", "multipart/form-data"형식을 처리해준다. 즉, content-type에 따라 request.data의 파싱방법이 달라진다.
    - DEFAULT_RENDERER_CLASSES는 "application/json", "text/html" 형식으로 렌더링해준다.
    - 그리고 저러한 렌더러는 DEFAULT_CONTENT_NEGOTIATION_CLASS로 지정된 클래스가 해준다.

--- games/views.py
+++ games/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
+from rest_framework.decorators import api_view
 from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
 from rest_framework import status
+from rest_framework.response import Response
+
 from games.models import Game
 from games.serializers import GameSerializer
 
@@ -14,38 +15,37 @@ class JsonResponse(HttpResponse):
         super(JsonResponse, self).__init__(content, **kwargs)
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def game_list(request):
     if request.method == 'GET':
         games = Game.objects.all()
         game_serializer = GameSerializer(games, many=True)
-        return JsonResponse(game_serializer.data)
+        return Response(game_serializer.data)
+
     elif request.method == 'POST':
-        game_data = JSONParser().parse(request)
-        game_serializer = GameSerializer(data=game_data)
+        game_serializer = GameSerializer(data=request.data)
         if game_serializer.is_valid():
             game_serializer.save()
-            return JsonResponse(game_serializer.data, status=status.HTTP_201_CREATED)
-        return JsonResponse(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(game_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'POST'])
 def game_detail(request, pk):
     try:
         game = Game.objects.get(pk=pk)
     except Game.DoesNotExist:
-        return HttpResponse(status=status.HTTP_404_NOT_FOUND)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         game_serializer = GameSerializer(game)
-        return JsonResponse(game_serializer.data)
+        return Response(game_serializer.data)
     elif request.method == 'PUT':
-        game_data = JSONParser().parse(request)
-        game_serializer = GameSerializer(game, data=game_data)
+        game_serializer = GameSerializer(game, data=request.data)
         if game_serializer.is_valid():
             game_serializer.save()
-            return JsonResponse(game_serializer.data)
-        return JsonResponse(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(game_serializer.data)
+        return Response(game_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         game.delete()
-        return HttpResponse(status=status.HTTP_204_NO_CONTENT)
+        return Response(status=status.HTTP_204_NO_CONTENT)
