2e0daf1cb8b3f2cc903688b63dc07a22f807ad27
unknown <profile2697@gmail.com>
2018-03-04 22:15:37 +0900

    migrate to postgresql

--- gamesapi/settings.py
+++ gamesapi/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'gamesapi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'games',
+        'USER': 'games',
+        'PASSWORD': 'games1234',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
