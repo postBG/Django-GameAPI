80ed4c17eeb1b0975e127dc220c0dc829bea217b
unknown <profile2697@gmail.com>
2018-03-10 17:04:11 +0900

    using user defined pagination

--- gamesapi/settings.py
+++ gamesapi/settings.py
@@ -122,6 +122,6 @@ STATIC_URL = '/static/'
 
 # RestFramework
 REST_FRAMEWORK = {
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'games.pagination.LimitOffsetPaginationWithMaxLimit',
     'PAGE_SIZE': 5
 }
